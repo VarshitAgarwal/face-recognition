47c22f7e855f711433939bc02402fbec57cee062
varshit <40146571+VarshitAgarwal@users.noreply.github.com>
2019-01-13 16:36:11 +0000

    jupyter-notebook

---
+++ gmail_Attachment.py
@@ -0,0 +1,62 @@
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+   
+fromaddr = "varshitagarwal@gmail.com"
+toaddr = "varshitagarwal@gmail.com"
+   
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+  
+# storing the senders email address   
+msg['From'] = fromaddr 
+  
+# storing the receivers email address  
+msg['To'] = toaddr 
+  
+# storing the subject  
+msg['Subject'] = "Login Alert in Your System"
+  
+# string to store the body of the mail 
+body = "Intruder image Capture Attach Below"
+  
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+  
+# open the file to be sent  
+filename = "face10.jpg "
+attachment = open("/root/new/face10.jpg", "rb") 
+  
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+  
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+  
+# encode into base64 
+encoders.encode_base64(p) 
+   
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+  
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login(fromaddr, "9001096018") 
+  
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+  
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+  
+# terminating the session 
+s.quit() 
